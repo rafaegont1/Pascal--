25f35e153569408ed6c04e2644f8104750db2a42
rafaegont1 <rafaelg000@gmail.com>
2025-04-16 10:36:16 -0300

    novos exemplos; existem erros quando o lexer passa no programa hex.pas

---
+++ examples/hex.pas
@@ -0,0 +1,9 @@
+program hex;
+
+var
+    num: integer;
+
+num := 0x1F02;
+writeln ("num = ", num);
+
+end.
---
+++ examples/sum.pas
@@ -0,0 +1,13 @@
+program sum;
+
+var
+    num1, num2, sum: integer;
+
+write ("Digite o número 1: ");
+readln (num1);
+write ("Digite o número 2: ");
+readln (num2);
+
+writeln ("num1 + num2 = ", sum);
+
+end.
